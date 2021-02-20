770f94efcca0f9ffab049da583f650992895eebb
DinizEduardo <eduardo.diniz987@gmail.com>
2021-02-20 12:08:11 -0300

    Commit inicial

---
+++ src/test/java/br/com/zup/mercadolivre/mercadoLivre/MercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.mercadolivre.mercadoLivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
